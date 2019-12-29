de736bca5a4927176d357655ce29d38c98708436
Gal Schlezinger <gal@spitfire.co.il>
2019-12-29 10:00:56 +0200

    fmt

--- src/npm.rs
+++ src/npm.rs
@@ -4,18 +4,18 @@ use std::process::{Command, Stdio};
 pub fn install(cwd: impl AsRef<Path>) -> std::io::Result<()> {
     let status_code = if cfg!(windows) {
         Command::new("cmd")
-        .args(&["/C", "npm install"])
-        .current_dir(cwd)
-        .stdout(Stdio::inherit())
-        .stderr(Stdio::inherit())
-        .status()?
+            .args(&["/C", "npm install"])
+            .current_dir(cwd)
+            .stdout(Stdio::inherit())
+            .stderr(Stdio::inherit())
+            .status()?
     } else {
         Command::new("npm")
-        .arg("install")
-        .current_dir(cwd)
-        .stdout(Stdio::inherit())
-        .stderr(Stdio::inherit())
-        .status()?
+            .arg("install")
+            .current_dir(cwd)
+            .stdout(Stdio::inherit())
+            .stderr(Stdio::inherit())
+            .status()?
     };
 
     if status_code.success() {
